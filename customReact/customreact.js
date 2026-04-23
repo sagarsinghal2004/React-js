a87e754b7423e3e0b35c6d02fc603860488386ca
Sagar Singhal <sagarsinghal2004@gmail.com>
2026-04-23 12:40:23 +0530

    custom react and elements

---
+++ customReact/customreact.js
@@ -0,0 +1,36 @@
+
+const mainContainer = document.querySelector('#root');
+
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'click me to visit google'
+}
+
+//creating a element to render
+
+function customRender(reactElement,container){
+
+    /*
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href', reactElement.props.href)
+    domElement.setAttribute('target', reactElement.props.target)
+    container.appendChild(domElement)
+    */
+   
+   //using a loop to set attributes
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for(const prop in reactElement.props){
+        if(prop === 'children') continue;
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
+    container.appendChild(domElement)
+}
+customRender(reactElement, mainContainer)
+console.log("hello sagar");
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,8 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+
+//custom react elements
+
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'click me to visit google'
+// }
+
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">click me to visit google</a>
+)
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+    anotherElement
+  
 )
