35bbe7718fa0cdafe8cd0a9dab7775a563deda3e
Sagar Singhal <sagarsinghal2004@gmail.com>
2026-04-23 19:04:39 +0530

    Concept of Babel and Expressions

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-
+import React from 'react'
 
 //custom react elements
 
@@ -15,11 +15,22 @@ import App from './App.jsx'
 // }
 
 
-const anotherElement = (
-  <a href="https://google.com" target="_blank">click me to visit google</a>
+// const anotherElement = (
+//   <a href="https://google.com" target="_blank">click me to visit google</a>
+// )
+
+//creating a element using react.createElement which uses react syntax
+const reactElement = React.createElement(
+  'a',
+  {
+    href: 'https://google.com',
+    target: '_blank'
+  },
+  'click me to visit google'
 )
+
 createRoot(document.getElementById('root')).render(
   
-    anotherElement
+    reactElement
   
 )
